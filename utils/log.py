642a18f77a8f2d16514b9652d1f65a13eecb3f07
戒酒的李白 <670939375@qq.com>
2025-01-17 18:16:08 +0800

    log路径问题

--- utils/log.py
+++ utils/log.py
@@ -17,6 +17,7 @@ def create_logger(name, silent=False, to_disk=False, log_file=None):
         log.addHandler(ch)
     if to_disk:
         log_file = log_file if log_file is not None else strftime("log/log_%m%d_%H%M%S.txt", gmtime())
+        log_file = log_file.replace(":", "-")  # HACK：替换冒号为短横线，避免windows文件名问题
         if type(log_file) == list:
             for filename in log_file:
                 fh = logging.FileHandler(filename, mode='w')
